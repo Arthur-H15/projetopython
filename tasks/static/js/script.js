c393b0e25bdf264cf5d8666dac324053ff840da8
Arthur-H15 <81658735+Arthur-H15@users.noreply.github.com>
2021-07-01 16:13:47 -0300

    updade quinta

--- tasks/static/js/script.js
+++ tasks/static/js/script.js
@@ -1,1 +1,16 @@
-console.log('rodando ')
+/*verificaçao de exclusao*/ 
+
+
+$(document).ready(function(){
+    var botaoD = $('.delete');
+    $(botaoD).on('click',function(e){
+        e.preventDefault();
+        var dellink=$(this).attr('href');
+        var confirma=confirm('quer deletar esta tarefa');
+        if(confirma){
+            window.location.href=dellink;
+        }
+
+    });
+
+});
